f05362d7ff62b2aa524a3c7caa4f15a3b85c7806
SimSimSim <leivaprogramming@gmail.com>
2024-05-14 13:42:07 -0400

    Portfolio section

--- src/app/components/Portfolio.tsx
+++ src/app/components/Portfolio.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { FaExternalLinkAlt } from 'react-icons/fa';
 
 function Portfolio() {
   const freelanceProjects = [
@@ -6,39 +7,46 @@ function Portfolio() {
       logo: '/logo/madamesmithlogo.png',
       name: 'Madame Smith',
       link: 'https://madame-smith.netlify.app/',
+      description: 'Interactive Restaurant Menu'
     },
     {
       logo: '/logo/LogoAtie&Hatietspbg.png',
       name: 'Atie & Hatie',
       link: 'https://www.atiehatie.com/',
+      description: 'E-Commerce Solution'
     },
     {
       logo: '/logo/leivainvestinglogo.png',
       name: 'Leiva Investing Inc',
       link: 'https://leiva-investing-inc.fly.dev/',
+      description: 'Finance Blog'
     },
   ];
 
   const effetMonstreProjects = [
     {
-      logo: '/simprofile.jpg',
-      name: 'L\'Effet Monstre Project 1',
-      link: 'https://example.com',
+      logo: '/logo/pdq.jpeg',
+      name: 'Les Produits du Québec',
+      link: 'https://lesproduitsduquebec.com/',
+      description: 'Non-Profit Organization in Quebec'
     },
     {
-      logo: '/simprofile.jpg',
-      name: 'L\'Effet Monstre Project 2',
-      link: 'https://example.com',
+      logo: '/logo/ifivemelogo.png',
+      name: 'IFiveMe',
+      link: 'https://www.ifiveme.com/',
+      description: 'Virtual Business Cards'
     },
     {
-      logo: '/simprofile.jpg',
-      name: 'L\'Effet Monstre Project 3',
-      link: 'https://example.com',
+      logo: '/logo/mc2.png',
+      name: 'MC2',
+      link: 'https://mc2is.ca/',
+      description: 'Health Resource Center'
     },
     {
-      logo: '/simprofile.jpg',
-      name: 'L\'Effet Monstre Project 4',
-      link: 'https://example.com',
+      logo: '/logo/dx.jpeg',
+      name: 'DX',
+      link: 'https://dx.ca/',
+      description: 'E-Commerce Solution'
     },
   ];
 
@@ -55,21 +63,33 @@ function Portfolio() {
                 <div className="flex items-center justify-center mb-4">
                   <Image src={project.logo} alt={project.name} width={80} height={80} />
                 </div>
-                <h4 className="text-xl font-bold text-center">{project.name}</h4>
+                <h4 className="text-xl mb-2 font-bold text-center">{project.name}</h4>
+                <p className="text-sm text-center">{project.description}</p>
               </a>
             ))}
           </div>
         </div>
 
         <div>
-          <h3 className="text-3xl font-semibold mb-6">L'Effet Monstre</h3>
+          <h3 className="text-3xl font-semibold mb-6 flex items-center">
+            L'Effet Monstre
+            <a
+              href="https://www.effetmonstre.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="ml-2 text-blue-500 hover:text-blue-300 transition"
+            >
+              <FaExternalLinkAlt className="w-4 h-4" />
+            </a>
+          </h3>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
             {effetMonstreProjects.map((project, index) => (
               <a key={index} href={project.link} target="_blank" rel="noopener noreferrer" className="group block p-6 rounded-lg bg-gray-800 hover:bg-gray-700 transition">
                 <div className="flex items-center justify-center mb-4">
-                  <Image src={project.logo} alt={project.name} width={50} height={50} />
+                  <Image src={project.logo} alt={project.name} width={80} height={80} />
                 </div>
-                <h4 className="text-xl font-bold text-center">{project.name}</h4>
+                <h4 className="text-xl mb-2 font-bold text-center">{project.name}</h4>
+                <p className="text-sm text-center">{project.description}</p>
               </a>
             ))}
           </div>
