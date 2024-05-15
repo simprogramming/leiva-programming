19fc81842d48f97dab0d8ae5e8afa657d671eb78
SimSimSim <leivaprogramming@gmail.com>
2024-05-15 15:02:15 -0400

    Update Home.tsx

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -22,11 +22,16 @@ function Home() {
 
   return (
     <section id="home" className="relative h-screen pb-20">
-      <div className="absolute inset-0 flex flex-col items-center lg:items-start justify-start pt-[25%] lg:pt-[9%] lg:pl-[37%]">
+      <div className="absolute inset-0 flex flex-col items-center lg:items-start justify-start pt-[25%] lg:pt-[8%] lg:pl-[37%]">
         <h1 className="text-white text-5xl md:text-5xl lg:text-6xl font-bold text-center lg:text-left">Simon Leiva</h1>
         <p className="text-white text-3xl font-medium mt-4 text-center lg:text-left relative">
           I&apos;m <span className="typed relative inline-block border-b-2 border-blue-500"></span>
         </p>
+      </div>
+      <div className="absolute bottom-20 lg:bottom-10 w-full flex justify-center animate-bounce">
+        <svg className="w-20 h-20 text-white lg:text-blue-900" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path>
+        </svg>
       </div>
       <style jsx>{`
         .typed::after {
